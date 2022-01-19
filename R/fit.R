ba604968a1344d3ac7cc0504b70fc9e1ced20e16
Peter Winskill <p.winskill@imperial.ac.uk>
2022-01-19 13:01:01 +0000

    Update fitting vignette

--- R/fit.R
+++ R/fit.R
@@ -30,6 +30,7 @@ fit_usage_objective <- function(distribution, distribution_timesteps,
 #' @param population Population
 #' @param net_loss Net loss function
 #' @param timesteps Timesteps
+#' @param rho betwen round correlation parameters
 #' @param seed Seed - the output of population_usage is stochastic.
 #' 
 #' 
@@ -45,13 +46,29 @@ fit_usage <- function(target_usage, target_usage_timesteps,
                       rho = 0,
                       seed = 1234){
   
+  if(any(distribution_lower < 0) | any(distribution_lower > 1)){
+    stop("All distribution lower values must be bewteen 0 and 1")
+  }
+  if(any(distribution_upper < 0) | any(distribution_upper > 1)){
+    stop("All distribution upper values must be bewteen 0 and 1")
+  }
+  if(length(distribution_lower) != length(distribution_timesteps) | length(distribution_upper) != length(distribution_timesteps)){
+    stop("distribution_timesteps, distribution_lower and distribution_upper must have equal lengths")
+  }
+  if(timesteps < max(target_usage_timesteps) | timesteps < max(distribution_timesteps)){
+    stop("Set timesteps to simulate to be greater than the maximum target usage timestep and distribution_timestep")
+  }
+  if(length(target_usage) != length(target_usage_timesteps)){
+    stop("Target usage and target usage timesteps must be the same length")
+  }
+  
   nloptr::cobyla(x0 = rep(0, length(distribution_timesteps)),
                  fn = fit_usage_objective,
                  lower = distribution_lower,
                  upper = distribution_upper,
                  distribution_timesteps = distribution_timesteps,
-                 target_usage = target, 
-                 target_usage_timesteps = target_tt,
+                 target_usage = target_usage, 
+                 target_usage_timesteps = target_usage_timesteps,
                  net_loss = net_loss,
                  population = population,
                  timesteps = timesteps,
