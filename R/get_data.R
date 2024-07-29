c571298a2515b786fb1a1a101bdbdb86e031b547
Peter Winskill <p.winskill@imperial.ac.uk>
2024-07-29 09:45:03 +0100

    Better handling of get data for countries not included in dataset

--- R/get_data.R
+++ R/get_data.R
@@ -1,19 +1,22 @@
 #' Get half life
 #'
-#' @param iso3c ISO3c code. If NA will return median half life estimate
+#' @param iso3c ISO3c code.
+#' @param not_found_fn A function to summarise across all
+#' available half-life estimates to produce an estimate
+#' for countries not in the dataset
+#' @param ... Further arguments passed to not_found_fn
 #'
 #' @export
 #' @return Half life
-get_halflife <- function(iso3c = NA){
+get_halflife <- function(iso3c, not_found_fn = stats::median, ...){
   hld <- netz::halflife
   
-  if(is.na(iso3c)){
-    half_life <- stats::median(hld$half_life)
-  } else {
-    if(!iso3c %in% hld$iso3c){
-      stop("ISO code not found")
-    }
+  iso_in_dataset <- iso3c %in% hld$iso3c
+  
+  if(iso_in_dataset){
     half_life <- hld[hld$iso3c == iso3c, "half_life"]
+  } else {
+    half_life <- not_found_fn(hld$half_life, ...)
   }
   
   return(half_life)
@@ -21,20 +24,23 @@ get_halflife <- function(iso3c = NA){
 
 #' Get usage rate
 #'
-#' @param iso3c ISO3c code. If NA will return median usage rate estimate
-#'
+#' @param iso3c ISO3c code.
+#' @param not_found_fn A function to summarise across all
+#' available half-life estimates to produce an estimate
+#' for countries not in the dataset
+#' @param ... Further arguments passed to not_found_fn
+#' 
 #' @export
 #' @return Usage rate
-get_usage_rate <- function(iso3c = NA){
+get_usage_rate <- function(iso3c, not_found_fn = stats::median, ...){
   ur <- netz::usage_rate
   
-  if(is.na(iso3c)){
-    usage_rate <- stats::median(ur$usage_rate)
-  } else {
-    if(!iso3c %in% ur$iso3c){
-      stop("ISO code not found")
-    }
+  iso_in_dataset <- iso3c %in% ur$iso3c
+  
+  if(iso_in_dataset){
     usage_rate <- ur[ur$iso3c == iso3c, "usage_rate"]
+  } else {
+    usage_rate <- not_found_fn(ur$usage_rate, ...)
   }
   
   return(usage_rate)
