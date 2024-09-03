ed16e8a608cf1e35b1218bbbaf097e16f42d97dc
Peter Winskill <p.winskill@imperial.ac.uk>
2024-09-03 09:49:44 +0100

    Model usage testing

--- tests/testthat/test-malariasimulation.R
+++ tests/testthat/test-malariasimulation.R
@@ -3,6 +3,13 @@ test_that("malariasimulation usage fitting works", {
   tut <- 365
   dt <- 365
   
+  expect_equal(usage_to_model_distribution(0, tut, dt), 0)
+  expect_equal(usage_to_model_distribution(1, tut, dt), 1)
+  expect_equal(usage_to_model_distribution(c(1,1), (1:2) * 365, (1:2) * 365), c(1, 1))
+  expect_equal(usage_to_model_distribution(c(0,0), (1:2) * 365, (1:2) * 365), c(0, 0))
+  expect_equal(usage_to_model_distribution(c(1,0), (1:2) * 365, (1:2) * 365), c(1, 0))
+  expect_equal(usage_to_model_distribution(c(1,1), (1:2) * 365, (1:3) * 365), c(1, 1, NA))
+  
   expect_error(usage_to_model_distribution(tu, tut, dt, distribution_lower = -1),
                "All distribution lower values must be between 0 and 1")
   expect_error(usage_to_model_distribution(tu, tut, dt, distribution_lower = 6), 
